d5f7af5a830da360021791413f2d0737e0f9b8e8
shivam gupta <shivamgupta2203@gmail.com>
2017-03-17 21:09:02 +0530

    Add files via upload

---
+++ storage/framework/views/55a8f53a7e4842e5361da7a177df338daa96b5f9.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(config('app.locale')); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Options</title>
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<style type="text/css">
+    body {
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+    }
+    main {
+        flex: 1 0 auto;
+    }
+</style>
+<body>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+
+<header>
+    <nav>
+        <div class="container">
+            <style class="brand-logo">Materialize</style>
+        </div>
+    </nav>
+</header>
+<main>
+    <div clas="container col-sm-12">
+        
+        <table class="table bordered highlight responsive-table centered">
+            <form method="post" action="payment">
+                <thead>
+                <tr>
+                    <th data-field="name">Name</th>
+                    <th data-field="description">Description</th>
+                    <th data-field="price">Price</th>
+                    <th data-field="image">image</th>
+                    <th data-field="select">select</th>
+                </tr>
+                </thead>
+                <?php for($i = 0; $i < $options->count()-1; $i++): ?>
+                    <tr>
+                        <td class="col-sm-2"><?php echo e($options[$i]->name); ?></td>
+                        <td class="col-sm-3"><?php echo e($options[$i]->description); ?></td>
+                        <td class="col-sm-2"><?php echo e($options[$i]->price); ?></td>
+                        <td class="col-sm-3"><img src="images/<?php echo e($options[$i]->img_path); ?>" height="100" width="150"></td>
+                        <td class="col-sm-2">
+                            <input type="radio" name="choice" id="<?php echo e($options[$i]->id); ?>" value="<?php echo e($options[$i]->id); ?>">
+                            <label for="<?php echo e($options[$i]->id); ?>"></label>
+                        </td>
+                    </tr>
+                <?php endfor; ?>
+                <tr>
+                    <td class="col-sm-2"><?php echo e($options[$options->count()-1]->name); ?></td>
+                    <td class="col-sm-3"><?php echo e($options[$options->count()-1]->description); ?></td>
+                    <td class="col-sm-2"></td>
+                    <td class="col-sm-2"></td>
+
+                    
+                    <td class="col-sm-2">
+                        <input type="radio" name="choice" id="100" value="100" checked>
+                        <label for="100"></label>
+                    </td>
+                </tr>
+                
+                <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>">
+        </table>
+            <div class="center">
+                <button class=" btn waves-effect waves-light" type="submit" name="action" value="choice">Submit
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </form>
+
+
+        
+    </div>
+</main>
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col s6">
+                <h5 class="white-text">Footer Content</h5>
+            </div>
+            <div class="col l4 offset-l2 s6">
+                <h5 class="white-text">Links</h5>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ storage/framework/views/8d49bde77f69e5a93e9d5ca56d86d2c603fbe4fd.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(config('app.locale')); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login</title>
+        <!-- Fonts -->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    </head>
+    <style type="text/css">
+        body {
+            display: flex;
+            min-height: 100vh;
+            flex-direction: column;
+        }
+        main {
+            flex: 1 0 auto;
+        }
+    </style>
+        <body>
+    <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+
+        <header>
+            <nav>
+                <div class="container">
+                    <div class="nav-wrapper">
+                        <style class="brand-logo">Materialize</style>
+                        <ul class="right side-nav" id="nav-mobile">
+                            <li class="hide-on-small-only"><a href="parallax.html"><i class="mdi-navigation-arrow-back"></i></a></li>
+                        </ul>
+                        <a class="button-collapse" href="#" data-activates='nav-mobile'><i class="mdi-navigation-menu"></i></a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container content col s6">
+                    <form class="col s6" method="post" action="login">
+                        <div class="input-field col s6">
+                            <input type="text" id="webmail" name="webmail" class="validate">
+                            <label for="webmail">Webmail ID</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input type="password" id="password" name="password" class="validate">
+                            <label for="password " >password</label>
+                        </div>
+
+                        <div>
+                            <label for="course" >Course</label>
+                            <select class="browser-default" id="course" name="course" >
+                                <option value="undergraduates" selected>Undergraduates</option>
+                                <option value="masters">Masters</option>
+                                <option value="phd">PhD</option>
+                            </select>
+                        </div>
+                        <br>
+                        <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>">
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                             <i class="material-icons right">send</i>
+                        </button>
+                        <div>
+                            <ul>
+                                <li>* Only for Graduating Students </li>
+                            <?php if(count($errors) > 0): ?>
+                                <div class="alert alert-danger">
+                                        <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                            <li>* <?php echo e($error); ?></li>
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </div>
+                            <?php endif; ?>
+                            </ul>
+                        </div>
+                    </form>
+                </div>
+
+        </main>
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">Footer Content</h5>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                        <h5 class="white-text">Links</h5>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
+
+
+
+        
+    
+        
+        
+        
+        
+        
