013d411d2edcbc6c6915d538b58815dff445e586
shivam gupta <shivamgupta2203@gmail.com>
2017-03-17 21:05:38 +0530

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('webmail')->unique();
+            $table->string('type');
+            $table->integer('choice')->default('0');
+            $table->boolean('mail_sent')->default(false);
+            $table->boolean('donation_via_transaction')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
